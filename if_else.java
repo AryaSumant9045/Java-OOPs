1bd2c4e50b2afd0dfd1d6534678b206737105a82
AryaSumant9045 <studywithpwno.1@gmail.com>
2025-09-16 20:27:57 +0530

    auto update 2025-09-16 20:27:57

--- if_else.java
+++ if_else.java
@@ -3,7 +3,7 @@ public class IfElseDemo {
         int number = 10;
 
         if (number > 0) {
-            System.out.println("The number is positive number.");
+            System.out.println("The number is positive.");
         } else {
             System.out.println("The number is not positive.");
         }
