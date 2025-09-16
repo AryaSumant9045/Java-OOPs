f1e567dd68541013d378a60d678bc440ba285d02
AryaSumant9045 <studywithpwno.1@gmail.com>
2025-09-16 18:54:42 +0530

    ADDED JAVA LAB

---
+++ lab 1/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+// Java mein agar class public hai, toh file ka naam class ke naam jaisa hi hona chahiye. Agar aap file ka naam alag rakhte hain, toh error aayeg a..8. 
+
+//Java program compile hone ke baad Bytecode banta hai.
+
+//JVM Bytecode ko interpret karke run karta hai (line-by-line execution).
+
+//Saath hi JIT compiler use hota hai jo speed improve karta hai.
